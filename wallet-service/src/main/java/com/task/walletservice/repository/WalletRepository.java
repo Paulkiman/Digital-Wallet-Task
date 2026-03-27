6ba602f29372b66bb4899fe9337a588be3bdbbfc
PaulNjoguEMTH <p.njogu@emtechhouse.co.ke>
2026-03-27 09:06:52 +0300

    Repository changes

--- wallet-service/src/main/java/com/task/walletservice/repository/WalletRepository.java
+++ wallet-service/src/main/java/com/task/walletservice/repository/WalletRepository.java
@@ -4,11 +4,9 @@ import com.task.walletservice.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
-    Optional<Wallet> findByOwnerEmail(String ownerEmail);
     boolean existsByOwnerEmail(String ownerEmail);
 }
